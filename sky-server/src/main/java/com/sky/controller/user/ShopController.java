9737273ae56bf6899eb0f43f6d8ad632aa622304
张春波 <3077366612@qq.com>
2026-05-21 19:57:36 +0800

    状态设置

---
+++ sky-server/src/main/java/com/sky/controller/user/ShopController.java
@@ -0,0 +1,25 @@
+package com.sky.controller.user;
+
+import com.sky.result.Result;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.*;
+
+@RestController("userShopController")
+@RequestMapping("/user/shop")
+@Slf4j
+@Api(tags = "店铺管理接口")
+public class ShopController {
+    @Autowired
+    private RedisTemplate redisTemplate;
+@ApiOperation( "获取店铺营业状态")
+@GetMapping("/status")
+public Result<Integer> getStatus(){
+    Integer shopStatus =(Integer) redisTemplate.opsForValue().get("SHOP_STATUS");
+    log.info("获取店铺营业状态{}",shopStatus == 1?"营业中":"打烊中");
+    return Result.success(shopStatus);
+}
+}
